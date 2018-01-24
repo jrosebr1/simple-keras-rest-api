014fcbafd577b6486ee3b9327133848bf5440379
Adrian Rosebrock <jrosebr1@umbc.edu>
2018-01-24 08:58:14 -0500

    Initial commit

---
+++ simple_request.py
@@ -0,0 +1,28 @@
+# USAGE
+# python simple_request.py
+
+# import the necessary packages
+import requests
+
+# initialize the Keras REST API endpoint URL along with the input
+# image path
+KERAS_REST_API_URL = "http://localhost:5000/predict"
+IMAGE_PATH = "dog.jpg"
+
+# load the input image and construct the payload for the request
+image = open(IMAGE_PATH, "rb").read()
+payload = {"image": image}
+
+# submit the request
+r = requests.post(KERAS_REST_API_URL, files=payload).json()
+
+# ensure the request was sucessful
+if r["success"]:
+	# loop over the predictions and display them
+	for (i, result) in enumerate(r["predictions"]):
+		print("{}. {}: {:.4f}".format(i + 1, result["label"],
+			result["probability"]))
+
+# otherwise, the request failed
+else:
+	print("Request failed")
